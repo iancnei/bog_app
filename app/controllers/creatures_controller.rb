fa2bb53b7caff8e250a3e62d0d5be17b04139ecc
inei <iancnei@gmail.com>
2015-09-18 17:04:29 -0700

    create new creature form, method, and route

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -5,4 +5,14 @@ class CreaturesController < ApplicationController
 		render :index
 	end
 
+	def new
+		render :new
+	end
+
+	def create
+		new_creature = params.require(:creature).permit(:name, :description)
+		Creature.create(new_creature)
+		redirect_to "/creatures"
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  root to: 'creatures#index' # same as 'get "/" => "creatures#index"'
   get '/creatures', to: "creatures#index" # same as 'get "/creatures" => "creatures#index"'
+
+  get '/creatures/new', to: "creatures#new"
+
+  post '/creatures', to: "creatures#create"
 
 end
